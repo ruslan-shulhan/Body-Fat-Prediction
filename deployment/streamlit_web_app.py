374d21315774b213ebe8e96a07eade65a2bd9a81
Ruslan Shulhan <ruslan.shulhan@gmail.com>
2022-03-08 13:52:25 -0500

    added two modules for LR model and StreamLit web_app

---
+++ deployment/streamlit_web_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+class StreamLit(object):
+    def __init__(self):
+        pass
+
+def set_title(self, app_title=""):
+    st.title()
+    st.write
+
+    return None
+
+def set_write(self, app_desciption):
+    st.write(app_desciption)
+
+    return None
